8efe73e1edfb6728376353bcf47466e1b4302d35
Davide Parini <davide.parini@gmail.com>
2018-08-27 16:47:25 +0200

    Aggiornato script pulizia.

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 set -e -x
 
 echo \#\#\# STOPPING CONTAINERS
-docker stop $(docker ps -a -q -f "name=pcxchg-")
+docker stop $(docker ps -a -q -f "name=dev-")
 
 echo \#\#\# DELETING CONTAINERS
-docker rm   $(docker ps -a -q -f "name=pcxchg-")
+docker rm   $(docker ps -a -q -f "name=dev-")
 
 echo \#\#\# DELETEING IMAGES
-docker rmi  $(docker images --format "{{.ID}}" -f "reference=*pcxchg-*")
+docker rmi  $(docker images --format "{{.ID}}" -f "reference=dev-*")
